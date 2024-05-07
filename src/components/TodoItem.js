1682362483d2f02b7375ad590f2b92abed746566
Rohan Gupta <7039426154rg@gmail.com>
2024-05-07 22:29:05 +0530

    minor change

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,9 @@ const TodoItem = () => {
           defaultValue={item.item} 
           onKeyPress={(e) => update(item.id , inputRef.current.value , e)} 
         ></textarea>
-      
+      <button onClick={() => changeFocus()}>Edit</button>
+      <button onClick={() => props.completeTodo(item.id)}>complete</button> 
+      <button onClick={() => props.removeTodo(item.id)}>Delete</button> 
     </li>
   )
 }

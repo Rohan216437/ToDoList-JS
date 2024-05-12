703475de1986ced93cd1e07131d546a7f9774036
Rohan Gupta <7039426154rg@gmail.com>
2024-05-12 21:51:57 +0530

    app file changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Todos from "./components/Todos";
+import TodoItem from './components/TodoItem'
 
 function App() {
   return (
     <div className="App">
       <Todos />
+      <TodoItem />
+
     </div>
   );
 }

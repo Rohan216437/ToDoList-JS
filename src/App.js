56b573438ebe12ab1a8ed96a11ad3c4fc502290b
Rohan Gupta <7039426154rg@gmail.com>
2024-05-13 22:11:34 +0530

    app file minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Todos from "./components/Todos";
 import TodoItem from './components/TodoItem'
+import DisplayTodo from "./components/DisplayTodo";
 
 function App() {
   return (
     <div className="App">
       <Todos />
-      <TodoItem />
+      <DisplayTodo />
 
     </div>
   );

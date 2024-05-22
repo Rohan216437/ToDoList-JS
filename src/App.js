eba48ae1d9ef3d8d5cf826944b45ac19f367b121
Rohan Gupta <72848796+Rohan216437@users.noreply.github.com>
2024-05-22 14:48:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import DisplayTodo from "./components/DisplayTodo";
 
 function App() {
   return (
-    <div className="mt-12 flex flex-col">
-      <h1 className="text-3xl inline text-center mb-8 text-white">Todo App</h1>
+    <div className="mt-6 w-screen flex flex-col">
+      <h1 className="text-3xl inline text-center mb-4 text-white">Todo App</h1>
       <Todos />
       <DisplayTodo />
     </div>

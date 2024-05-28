7a1bf4eceb450f7192dc1db6446c411fc44f37cc
Rohan Gupta <7039426154rg@gmail.com>
2024-05-28 23:08:44 +0530

    streak 28thMay

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -46,13 +46,13 @@ const Todos = (props) => {
           <div class="w-full sm:mb-2">
             <label for="input1">
               <span class="ml-2 text-sm text-white sm:text-base ">Add you Todo's !!</span>
-                <input id="input1" minlength="5" className="mt-1 py-3 px-5 w-full border-2 border-white rounded-2xl outline-none placeholder:text-gray-400 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-skyblue-500 peer dark:bg-white dark :text-gray-200 dark:placeholder:text-gray-300 dark:invalid:text-rose-600 dark:border-gray-400"
+                <input id="input1" minlength="5" className="mt-1 py-3 px-5 w-full border-2 border-white rounded-2xl outline-none placeholder:text-gray-400 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-skyblue-500 peer dark:bg-white dark :text-gray-200 dark:placeholder:text-gray-300 dark:invalid:text-rose-600 dark:border-gray-400 shadow-md"
                 type="text" onChange={(e) => {handleChange(e)}} placeholder="Type something" />
               <p class="ml-2 text-xs text-pink-700 invisible peer-invalid:visible dark:text-gray-200">less than 5 characters</p>
           </label>
         </div>
         <div 
-          class="w-full text-center py-3 px-8 text-sm font-medium bg-cyan-800 text-gray-100 rounded-2xl cursor-pointer sm:w-min hover:bg-cyan-900 hover:text-gray-50  dark:text-gray-100 dark:hover:text-gray-50 mb-4 sm:mb-0"
+          class="w-full text-center py-3 px-8 text-sm font-medium bg-cyan-800 text-gray-100 rounded-2xl cursor-pointer sm:w-min hover:bg-cyan-900 hover:text-gray-50  dark:text-gray-100 dark:hover:text-gray-50 mb-4 sm:mb-0 shadow-xl"
           onClick={() => props.addTodo({
             //object /todo write here
             id: Math.floor(Math.random()*1000),

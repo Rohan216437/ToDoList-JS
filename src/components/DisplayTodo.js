46d6c1480224d43e68d1491f4f97b097b05eb7f8
Rohan Gupta <7039426154rg@gmail.com>
2024-05-30 22:59:36 +0530

    motion css variable added

--- src/components/DisplayTodo.js
+++ src/components/DisplayTodo.js
@@ -24,6 +24,17 @@ const DisplayTodo = (props) => {
 
     const [sort , setSort] = useState("active")
 
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+          }
+        }
+      };
     
 
   return (  
